7472972c334d6be773038cbd21ac5d0947c8b5df
Dreagonmon <531486058@qq.com>
2021-08-17 10:47:34 +0800

    add no_psram version play32

--- ports/esp32/boards/PLAY32_PSRAM/mpconfigboard.cmake
+++ ports/esp32/boards/PLAY32_PSRAM/mpconfigboard.cmake
@@ -1,7 +1,8 @@
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
-    boards/PLAY32/sdkconfig.board
+    boards/sdkconfig.spiram
+    boards/PLAY32_PSRAM/sdkconfig.board
 )
 
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
