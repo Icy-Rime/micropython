48b9d6505f1b7f2a4e0a41f0f42d0817686d5f23
Dreagonmon <531486058@qq.com>
2023-05-15 20:40:45 +0800

    unix: add play32 variant

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 # Changes from MicroPython
 - ports/esp32: add board PLAY32 and PLAY32_PSRAM
+- ports/unix: add play32 variant
---
+++ ports/unix/variants/play32/mpconfigvariant.mk
@@ -0,0 +1,5 @@
+# This is the default variant when you `make` the Unix port.
+
+FROZEN_MANIFEST ?= $(VARIANT_DIR)/manifest.py
+MICROPY_PY_FFI = 1
+MICROPY_STANDALONE = 1
